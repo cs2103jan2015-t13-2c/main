2a1656308310a142c560d31a06fab89cf0efe3c8
Lee Kai Yi <kaiyisg@yahoo.com.sg>
2015-04-11 15:40:27 +0800

    updating TaskDisplayer in case need to use

--- Logic/TaskDisplayer.cpp
+++ Logic/TaskDisplayer.cpp
@@ -15,57 +15,126 @@ string TaskDisplayer::parseAllTasksToDisplay(){
 	TaskManager* instance = TaskManager::getInstance();
 
 	vector<Task>* allCurrentTasks = instance->getAllCurrentTasks();
+	vector<Task>* allTimedTasks = instance->getAllTimedTasks();
+	vector<Task>* allFloatingTasks = instance->getAllFloatingTasks();
+	vector<Task>* allMarkedTimedTasks = instance->getAllMarkedTimedTasks();
+	vector<Task>* allMarkedFloatingTasks = instance->getAllMarkedFloatingTasks();
 
-	ostringstream taskToDisplay;
+	ostringstream allTasksToDisplay;
 
 	vector<Task>::iterator iterTemp;
 
 	//takes care of the case where there is only 0 task
 	if (allCurrentTasks->size() == 0){
-		return "";
+		return "There are no current tasks, add a task!";
 	}
 
-	//should already be arranged in order
-	//this takes care of the case where by all current tasks has no deadline or timed task at all!
-	Task firstTask = allCurrentTasks->at(0);
-	if (!(firstTask.getTaskType() == Task::FLOATING)){
-		taskToDisplay << parseDateToDisplay(firstTask) << "\n";
+	//iterating through the timed / deadline tasks
+	for (iterTemp = allTimedTasks->begin(); iterTemp != allTimedTasks->end(); ++iterTemp) {
+
+		Task currentTask = *iterTemp;
+
+		if (currentTask.getTaskType() == Task::TIMED){
+
+			allTasksToDisplay << parseTimedTaskToDisplay(currentTask);
+
+		}
+		else{
+
+			allTasksToDisplay << parseDeadlineTaskToDisplay(currentTask);
+
+		}
 	}
 
-	//takes care of the case where there is only 1 task
-	if (allCurrentTasks->size() == 1){
-		taskToDisplay << parseOneTaskToDisplay(firstTask);
-	}
+	//iterating through floating task
+	for (iterTemp = allFloatingTasks->begin(); iterTemp != allFloatingTasks->end(); ++iterTemp) {
 
-	//iterating through 2 or more tasks to display
-	for (iterTemp = allCurrentTasks->begin(); iterTemp != allCurrentTasks->end(); ++iterTemp) {
+		Task currentTask = *iterTemp;
 
-		//if ();
+		allTasksToDisplay << parseFloatingTaskToDisplay(currentTask);
 
 	}
 
-	return "hello";
+	//iterating through marked deadline tasks
+
+	for (iterTemp = allMarkedTimedTasks->begin(); iterTemp != allMarkedTimedTasks->end(); ++iterTemp) {
+
+		Task currentTask = *iterTemp;
+
+		if (currentTask.getTaskType() == Task::TIMED){
+
+			allTasksToDisplay << parseTimedTaskToDisplay(currentTask);
+
+		}
+		else{
+
+			allTasksToDisplay << parseDeadlineTaskToDisplay(currentTask);
+
+		}
+	}
+
+	//iterating through marked floating task
+	for (iterTemp = allMarkedFloatingTasks->begin(); iterTemp != allMarkedFloatingTasks->end(); ++iterTemp) {
+
+		Task currentTask = *iterTemp;
+
+		allTasksToDisplay << parseFloatingTaskToDisplay(currentTask);
+
+	}
+	
+
+	return allTasksToDisplay.str();
 }
 
-string TaskDisplayer::parseOneTaskToDisplay(Task currentTask){
-	
+string TaskDisplayer::parseTimedTaskToDisplay(Task timedTask){
+
 	ostringstream taskToDisplay;
-	
-	taskToDisplay << "get stuff from display!";
-	
-	return "hello";
+
+	string startDate = parseDateToDisplay(timedTask.getTaskStartTime());
+	string endDate = parseDateToDisplay(timedTask.getTaskEndTime());
+	string task = timedTask.getTaskDetails();
+
+	taskToDisplay << startDate << " to " << endDate << ": " << task;
+
+	return taskToDisplay.str();
+
+}
+
+string TaskDisplayer::parseDeadlineTaskToDisplay(Task deadlineTask){
+
+	ostringstream taskToDisplay;
+
+	string deadlineDate = parseDateToDisplay(deadlineTask.getTaskDeadline());
+	string task = deadlineTask.getTaskDetails();
+
+	taskToDisplay << deadlineDate << " by: " << task;
+
+	return taskToDisplay.str();
+
+}
+
+string TaskDisplayer::parseFloatingTaskToDisplay(Task floatingTask){
+
+	ostringstream taskToDisplay;
+
+	string task = floatingTask.getTaskDetails();
+
+	taskToDisplay << "No deadline! | " <<task;
+
+	return taskToDisplay.str();
+
 }
 
 //assuming passing in a deadline task OR timed task
-string TaskDisplayer::parseDateToDisplay(Task currentTask){
+string TaskDisplayer::parseDateToDisplay(Date* date){
 
 	ostringstream dateToDisplay;
 
-	Date* currentDate = currentTask.getTaskDeadline();
-	currentDate = currentTask.getTaskStartTime();
+	Date* currentDate = date; 
 
-	dateToDisplay << "[" << currentDate->getDay() << "/" << currentDate->getMonth()
-		<< "/" << currentDate->getYear() << "] " << currentDate->getDayName();
+	dateToDisplay << currentDate->getDay() << "/" << currentDate->getMonth()
+		<< "/" << currentDate->getYear() << " " << currentDate->getHour() << ":" << 
+		currentDate->getMinute();
 
 	return dateToDisplay.str();
 }
--- Logic/TaskDisplayer.h
+++ Logic/TaskDisplayer.h
@@ -16,6 +16,11 @@ public:
 
 	static string parseOneTaskToDisplay(Task currentTasks);
 
-	static string parseDateToDisplay(Task currentTasks);
+	static string TaskDisplayer::parseDateToDisplay(Date* date);
+
+	static string TaskDisplayer::parseTimedTaskToDisplay(Task timedTask);
+	static string TaskDisplayer::parseDeadlineTaskToDisplay(Task deadlineTask);
+	static string TaskDisplayer::parseFloatingTaskToDisplay(Task floatingTask);
+
 };
 
